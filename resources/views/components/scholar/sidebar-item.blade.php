e2fb5dee17b559581de98a1216633b2d6d43489c
William Galas <williamgalas2@gmail.com>
2022-03-21 14:03:40 +0800

    Update sidebar-item.blade.php

--- resources/views/components/scholar/sidebar-item.blade.php
+++ resources/views/components/scholar/sidebar-item.blade.php
@@ -1,5 +1,5 @@
 @props(['href' => '#', 'label' => 'demo', 'active' => $href === url()->current()])
-<li class="uppercase font-bold">
+<li class="uppercase text-sm">
     <a href="{{$active ? "#":$href}}"
         class="h-12 {{$active ? 'bg-gray-900 text-white' : 'bg-gray-300'}} hover:bg-gray-700 hover:text-white flex items-center justify-center block w-full">
         {{$label}}
