7d9c441167441be1a86bc3b980a51f1e0e4c15d6
William Galas <williamgalas2@gmail.com>
2022-03-26 11:55:14 +0800

    Update work-card.blade.php

--- resources/views/components/scholar/work-card.blade.php
+++ resources/views/components/scholar/work-card.blade.php
@@ -1,6 +1,6 @@
 @props(['penname' => '', 'cover' => 'https://api.lorem.space/image/movie?w=200&h=280', 'href' => '#'])
 <div class="{{$penname}}">
-    <div style="background:url('{{$cover}}');background-position:center;background-size:cover;" class="border overflow-hidden relative  w-24 h-32 rounded m-2 mt-0 mb-1 hover:scale-110">
+    <div style="background:url('{{$cover}}');background-position:center;background-size:cover;" class="border overflow-hidden relative  w-24 h-32 rounded mr-2 mt-0 mb-1 hover:scale-110">
     </div>
     <div class="text-center w-24 text-xs">
         <a href="{{$href}}" class="hover:underline">
