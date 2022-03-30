1c32b8f4c7fedbe938ce245dee11bf94f2b4a9a6
William Galas <williamgalas2@gmail.com>
2022-03-31 03:17:42 +0800

    Update login.blade.php

--- resources/views/scholar/login.blade.php
+++ resources/views/scholar/login.blade.php
@@ -1,7 +1,7 @@
 <x-scholar.layout>
     <div class="flex justify-center">
         <form action="{{route('scholar.login')}}" method="POST" class="w-full md:w-8/12 mt-4">
-            <h1 class="text-2xl mb-4 uppercase text-center">Welcome Scholar</h1>
+            <h1 class="text-2xl mb-4 uppercase font-bold">Login</h1>
             @if (session()->get('error'))
                 <p class="font-bold text-red-600 text-xs text-center">
                     These credentials do not match our records.
