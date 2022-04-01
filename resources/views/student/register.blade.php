4541a9e82b133296cbb2bf9e7126b208b76c5402
William Galas <williamgalas2@gmail.com>
2022-04-02 01:10:47 +0800

    wip

--- resources/views/student/register.blade.php
+++ resources/views/student/register.blade.php
@@ -8,7 +8,17 @@
     <link href="/css/app.css" rel="stylesheet"/>
     <script src="/js/app.js" defer></script>
 </head>
-<body>
+<body class="relative h-screen">
     <x-student.topbar/>
+
+    <div
+    class="h-20 w-full bg-gray-900 absolute bottom-0 flex justify-center items-center px-4"
+    style="background:url('/students/nav/bottom-bg.png'); background-position:top center; background-size:cover;">
+        <img src="/students/nav/notification.png" alt="" class="w-16 mx-2">
+        <img src="/students/nav/diary.png" alt="" class="w-16 mx-2">
+        <img src="/students/nav/home.png" alt="" class="-top-5 w-28 relative">
+        <img src="/students/nav/collection.png" alt="" class="w-16 mx-2">
+        <img src="/students/nav/phone.png" alt="" class="w-16 mx-2">
+    </div>
 </body>
 </html>
