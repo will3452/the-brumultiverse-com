44a77b9b534a33ff7228913a7c674c9a3ab813ba
William Galas <williamgalas2@gmail.com>
2022-04-07 02:18:32 +0800

    Update ProfileController.php

--- app/Http/Controllers/Scholar/ProfileController.php
+++ app/Http/Controllers/Scholar/ProfileController.php
@@ -18,7 +18,7 @@ class ProfileController extends Controller
     public function update(Request $request, User $user)
     {
         $data = $request->validate([
-            'email' => ['required'],
+            'email' => ['required', 'unique:users,email,'.$user->id],
             // 'user_name' => ['required', "unique:users,user_name,$user->id"],
             'password' => ['required'],
             // 'email' => ['required', "unique:users,email,$user->id", "email"],
