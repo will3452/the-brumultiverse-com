4a0096bb01f1863bbb8cfea81bc0cede75dcd61e
William Galas <williamgalas2@gmail.com>
2022-04-07 06:35:23 +0800

    wip

--- app/Helpers/AssetHelper.php
+++ app/Helpers/AssetHelper.php
@@ -6,8 +6,11 @@ class AssetHelper
 {
     const BASE_URL = 'https://raw.githubusercontent.com/will3452/bru-assets/main/';
 
-    public static function get($path)
+    public static function get($path, $local=false)
     {
+        if ($local) {
+            return "/students/$path";
+        }
         return self::BASE_URL . $path;
     }
 }
