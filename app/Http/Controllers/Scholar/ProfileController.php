271ca518a1542840ce24f0205141ba8ddc98d1e5
William Galas <williamgalas2@gmail.com>
2022-04-07 02:24:26 +0800

    Update ProfileController.php

--- app/Http/Controllers/Scholar/ProfileController.php
+++ app/Http/Controllers/Scholar/ProfileController.php
@@ -25,7 +25,7 @@ class ProfileController extends Controller
             // 'address' => ['required'],
         ]);
 
-        if ($data['email'] !== $user->email) {
+        if ($data['email'] != $user->email) {
             $user->update(['email_verified_at' => null]);
         }
 
