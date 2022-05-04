049842fd321b1b82a54559ece1b95e950e39908a
William Galas <williamgalas2@gmail.com>
2022-05-04 12:33:49 +0800

    Update index.blade.php

--- resources/views/student/library/index.blade.php
+++ resources/views/student/library/index.blade.php
@@ -11,7 +11,6 @@
             @endforeach
         </x-student.work-container>
         @endforeach
-        <button id="modal-1">click me</button>
         <div class="mb-52"></div>
     </div>
     @push('head-script')
@@ -22,18 +21,5 @@
         <x-vendor.slickjs />
     @endpush
     @push('body-script')
-    <script>
-        document.getElementById('modal-1').onclick = function() {
-            Modal.open({
-                content: `<strong>Default modal!</strong>
-                        <br />Testing the modal.
-                        <br /><
-                        br />Loreum ipsum dolorem the quick brown
-                        fox jumped over the lazy dog.
-                        <br /><br />Yes its true.`,
-                draggable: true,
-            });
-            }
-    </script>
     @endpush
 </x-student.layout>
