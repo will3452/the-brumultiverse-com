0739ab06875e00bf7d9431006996e448e9a112e6
William Galas <williamgalas2@gmail.com>
2022-05-25 09:16:14 +0800

    Update intro.blade.php

--- resources/views/student/library/intro.blade.php
+++ resources/views/student/library/intro.blade.php
@@ -1,7 +1,7 @@
 <x-student.layout>
     <x-student.static-background-container bg="/students/library-assets/base.png" blur="1">
         <div>
-            <x-student.dialog-container>
+            <x-student.dialog-container :small="true">
                 <x-student.typing clear="0" message="Looking for any book in particular? I'm Julio, by the way." />
                 <x-student.dialog-button-container>
                     <a class="btn-student-active mx-2" href="{{route('student.library.index')}}">
