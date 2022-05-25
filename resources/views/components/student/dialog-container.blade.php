d015c73a4d35d4b10ea733db35358214b7cb80d1
William Galas <williamgalas2@gmail.com>
2022-05-25 09:14:24 +0800

    Update dialog-container.blade.php

--- resources/views/components/student/dialog-container.blade.php
+++ resources/views/components/student/dialog-container.blade.php
@@ -1,3 +1,3 @@
-<div class="backdrop-brightness-50 backdrop-blur-sm p-5 z-30 absolute w-full">
+<div class="backdrop-brightness-50 backdrop-blur-sm p-10 z-30 absolute w-full">
     {{$slot}}
 </div>
