00397bb0724bd15cd127a6df4b01ca2eb04737b2
will3452 <williamgalas2@gmail.com>
2022-06-05 00:05:36 +0800

    add loader

---
+++ resources/views/components/student/loader.blade.php
@@ -0,0 +1,8 @@
+<div id="loader-container">
+    <div class="loader book">
+        <figure class="page"></figure>
+        <figure class="page"></figure>
+        <figure class="page"></figure>
+      </div>
+      <h1 class="loader">Loading</h1>
+</div>
