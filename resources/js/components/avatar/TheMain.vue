22383d339ccdfe1d9c474678e016f137a37d138a
will3452 <williamgalas2@gmail.com>
2022-06-24 05:46:10 +0800

    Update TheMain.vue

--- resources/js/components/avatar/TheMain.vue
+++ resources/js/components/avatar/TheMain.vue
@@ -138,6 +138,9 @@ import ThumbnailVue from "./Thumbnail.vue";
         },
         computed: {
             currentBaseImage () {
+                if (! this.choices) {
+                    return '#'
+                }
                 let path = this.choices.bases.find((e)=>e.id == this.baseActive);
                 if (path == null) {
                     return '#';
@@ -145,6 +148,9 @@ import ThumbnailVue from "./Thumbnail.vue";
                 return this.uri + "/storage/" + path.path;
             },
             currentHairImage () {
+                if (! this.choices) {
+                    return '#'
+                }
                 let path = this.choices.hairstyles.find((e)=>e.id == this.hairActive);
                 if (path == null) {
                     return '#';
@@ -152,6 +158,9 @@ import ThumbnailVue from "./Thumbnail.vue";
                 return this.uri + "/storage/" + path.path;
             },
             currentClothesImage () {
+                if (! this.choices) {
+                    return '#'
+                }
                 let path = this.choices.clothes.find((e)=>e.id == this.clothesActive);
                 if (path == null) {
                     return '#';
