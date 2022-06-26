1f3c68967e4f8b4bfb831877f9a50506756ff9b5
will3452 <williamgalas2@gmail.com>
2022-06-26 11:40:18 +0800

    Update dialog-container.blade.php

--- resources/views/components/student/dialog-container.blade.php
+++ resources/views/components/student/dialog-container.blade.php
@@ -4,7 +4,7 @@
         {{$slot}}
     </div>
 @else
-    <div class="backdrop-brightness-50 backdrop-blur-sm p-10 z-30 absolute w-full">
+    <div class="@if($backdrop) backdrop-brightness-50 backdrop-blur-sm @endif p-10 z-30 absolute w-full">
         {{$slot}}
     </div>
 @endif
