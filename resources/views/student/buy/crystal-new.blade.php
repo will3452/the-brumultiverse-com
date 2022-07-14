5aed6b4d1db24665070aa733d734387254f6e177
will3452 <williamgalas2@gmail.com>
2022-07-14 19:49:41 +0800

    Update crystal-new.blade.php

--- resources/views/student/buy/crystal-new.blade.php
+++ resources/views/student/buy/crystal-new.blade.php
@@ -3,7 +3,7 @@
         <h1 class="text-center text-2xl uppercase bg-active py-5 mb-5">Packages available</h1>
         <div class="flex p-4 justify-center flex-wrap">
             @foreach ($packages as $p)
-                <div class="w-full md:w-2/12 mx-2">
+                <div class="w-full md:w-2/12 m-2">
                     <div class="border-2 p-5 rounded-2xl bg-gray-200 text-center">
                         <img src="/storage/{{$p->picture}}" alt="">
                         <div class="uppercase font-bold font-mono mt-2 ">
