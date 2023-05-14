76db0d74b3ed146ce79009a0217390aab535416a
will3452 <williamgalas2@gmail.com>
2023-05-14 12:39:36 +0800

    fix

--- app/Http/Controllers/EmailVerificationController.php
+++ app/Http/Controllers/EmailVerificationController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Foundation\Auth\RedirectsUsers;
 use Illuminate\Http\JsonResponse;
@@ -14,14 +13,6 @@ class EmailVerificationController extends Controller
     use RedirectsUsers;
     public function verify(Request $request, User $user)
     {
-        if (!hash_equals((string) $request->route('id'), (string) $user->getKey())) {
-            throw new AuthorizationException;
-        }
-
-        if (!hash_equals((string) $request->route('hash'), sha1($user->getEmailForVerification()))) {
-            throw new AuthorizationException;
-        }
-
         if ($user->hasVerifiedEmail()) {
             if ($user->isScholar()) {
                 return redirect(route('scholar.profile.show', ['user' => $user->id]));
