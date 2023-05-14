150291a7d7633d5068a2713d47d212795974ba6d
will3452 <williamgalas2@gmail.com>
2023-05-14 12:36:00 +0800

    Update EmailVerificationController.php

--- app/Http/Controllers/EmailVerificationController.php
+++ app/Http/Controllers/EmailVerificationController.php
@@ -3,53 +3,50 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use Laravel\Nova\Nova;
-use Illuminate\Http\Request;
-use Laravel\Nova\Actions\Action;
-use Illuminate\Http\JsonResponse;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Foundation\Auth\RedirectsUsers;
-use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class EmailVerificationController extends Controller
 {
     use RedirectsUsers;
     public function verify(Request $request, User $user)
     {
-        // if (! hash_equals((string) $request->route('id'), (string) $request->user()->getKey())) {
-        //     throw new AuthorizationException;
-        // }
-
-        if (! hash_equals((string) $request->route('hash'), sha1($request->user()->getEmailForVerification()))) {
+        if (!hash_equals((string) $request->route('id'), (string) $user->getKey())) {
             throw new AuthorizationException;
         }
 
-        if ($request->user()->hasVerifiedEmail()) {
-            if ($request->user()->isScholar()) {
-                return redirect(route('scholar.profile.show', ['user' => $request->user()->id]));
-            }
-            return $request->wantsJson()
-                        ? new JsonResponse([], 204)
-                        : redirect($this->redirectPath());
+        if (!hash_equals((string) $request->route('hash'), sha1($user->getEmailForVerification()))) {
+            throw new AuthorizationException;
         }
 
-        if ($request->user()->markEmailAsVerified()) {
-            event(new Verified($request->user()));
+        if ($user->hasVerifiedEmail()) {
+            if ($user->isScholar()) {
+                return redirect(route('scholar.profile.show', ['user' => $user->id]));
+            }
+            return $request->wantsJson()
+            ? new JsonResponse([], 204)
+            : redirect($this->redirectPath());
+        }
+
+        if ($user->markEmailAsVerified()) {
+            event(new Verified($user));
         }
 
         if ($response = $this->verified($request)) {
             return $response;
         }
 
-        if ($request->user()->isScholar()) {
-            return redirect(route('scholar.profile.show', ['user' => $request->user()->id]));
+        if ($user->isScholar()) {
+            return redirect(route('scholar.profile.show', ['user' => $user->id]));
         }
 
         return $request->wantsJson()
-                    ? new JsonResponse([], 204)
-                    : redirect($this->redirectPath())->with('verified', true);
+        ? new JsonResponse([], 204)
+        : redirect($this->redirectPath())->with('verified', true);
     }
-
 
     protected function verified(Request $request)
     {
@@ -58,13 +55,13 @@ class EmailVerificationController extends Controller
 
     public function resend(Request $request)
     {
-        if ($request->user()->hasVerifiedEmail()) {
+        if ($user->hasVerifiedEmail()) {
             return $request->wantsJson()
-                        ? new JsonResponse([], 204)
-                        : redirect($this->redirectPath());
+            ? new JsonResponse([], 204)
+            : redirect($this->redirectPath());
         }
 
-        $request->user()->sendEmailVerificationNotification();
+        $user->sendEmailVerificationNotification();
 
         return 'Verification link has been sent to your email!';
     }
